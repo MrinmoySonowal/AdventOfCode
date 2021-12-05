4de1defbc8e58c39cb03455debd6861a0ef9787f
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2021-12-05 13:37:28 +0000

    Changed size of matrix based on max values of coordinates

--- 2021/day_5/day_5.py
+++ 2021/day_5/day_5.py
@@ -11,12 +11,14 @@ class day_5:
         mat_size = 0
         with open(file_name) as f:
             line_segments = [inp_line.strip().split(' -> ') for inp_line in f.readlines()]
+        max_rows, max_cols = 0, 0
         for line in line_segments:
             x1, y1 = map(int, line[0].split(','))
             x2, y2 = map(int, line[1].split(','))
-            mat_size = max(mat_size, x1, x2, y1, y2)
+            max_cols = max(max_cols, x1, x2)
+            max_rows = max(max_rows, y1, y2)
             self.line_segments.append((x1, y1, x2, y2))
-        self.mat = np.zeros((mat_size + 1, mat_size + 1))
+        self.mat = np.zeros((max_rows + 1, max_cols + 1))
 
     def part_1(self):
         for line_pts in self.line_segments:
@@ -63,6 +65,8 @@ class day_5:
 
 
 if __name__ == '__main__':
-    d5 = day_5('input.txt')
+    d5 = day_5('test_input.txt')
+    print(d5.part_1())
+    d5 = day_5('test_input.txt')
     print(d5.part_2())
     d5.print_mat()
