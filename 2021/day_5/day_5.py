230df3b7e1e490d367165e30de2afb59ce21ae62
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2021-12-05 08:10:12 +0000

    Day 5 added test input, input and code

---
+++ 2021/day_5/day_5.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+class day_5:
+    def __init__(self, file_name):
+        self.line_segments = []
+        self.mat = []
+        self.assign_input(file_name)
+
+    def assign_input(self, file_name):
+        mat_size = 0
+        with open(file_name) as f:
+            line_segments = [inp_line.strip().split(' -> ') for inp_line in f.readlines()]
+        for line in line_segments:
+            x1, y1 = map(int, line[0].split(','))
+            x2, y2 = map(int, line[1].split(','))
+            mat_size = max(mat_size, x1, x2, y1, y2)
+            self.line_segments.append((x1, y1, x2, y2))
+        self.mat = np.zeros((mat_size + 1, mat_size + 1))
+
+    def part_1(self):
+        for line_pts in self.line_segments:
+            x1, y1, x2, y2 = line_pts
+            if x1 == x2:
+                row_idx_1 = min(y1, y2)
+                row_idx_2 = max(y1, y2)
+                self.mat[row_idx_1:row_idx_2 + 1, x1] += 1
+            elif y1 == y2:
+                col_idx_1 = min(x1, x2)
+                col_idx_2 = max(x1, x2)
+                self.mat[y1, col_idx_1:col_idx_2 + 1] += 1
+        return np.sum(self.mat >= 2)
+
+    def part_2(self):
+        for line_pts in self.line_segments:
+            x1, y1, x2, y2 = line_pts
+            if x1 == x2:
+                row_idx_1 = min(y1, y2)
+                row_idx_2 = max(y1, y2)
+                self.mat[row_idx_1:row_idx_2 + 1, x1] += 1
+            elif y1 == y2:
+                col_idx_1 = min(x1, x2)
+                col_idx_2 = max(x1, x2)
+                self.mat[y1, col_idx_1:col_idx_2 + 1] += 1
+            elif abs(x1 - x2) == abs(y1 - y2):  # diagonal case for 45
+                if x1 < x2 and y1 < y2:
+                    for i in range(abs(x1 - x2) + 1):
+                        self.mat[y1 + i][x1 + i] += 1
+                elif x1 > x2 and y1 > y2:
+                    for i in range(abs(x1 - x2) + 1):
+                        self.mat[y2 + i][x2 + i] += 1
+                elif x1 < x2 and y1 > y2:
+                    for i in range(abs(x1 - x2) + 1):
+                        self.mat[y1 - i][x1 + i] += 1
+                elif x1 > x2 and y1 < y2:
+                    for i in range(abs(x1 - x2) + 1):
+                        self.mat[y2 - i][x2 + i] += 1
+
+        return np.sum(self.mat >= 2)
+
+    def print_mat(self):
+        print(self.mat)
+
+
+if __name__ == '__main__':
+    d5 = day_5('input.txt')
+    print(d5.part_2())
+    d5.print_mat()
