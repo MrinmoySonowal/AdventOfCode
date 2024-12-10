dea6f14e7667493d62e1222997081aff43991fa0
MrinmoySonowal <mrinmoy.mly.sonowal@gmail.com>
2024-12-10 15:29:58 +0000

    Changed Grid implementation to take type as a parameter

--- utils/grid.py
+++ utils/grid.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
+from typing import Any, Callable
 
 Coord = namedtuple("Coord", ["x", "y"])
 
 class Grid:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, convert_to_type: Callable = lambda x: str(x)):
         with open(filename, 'r') as f:
-            raw = [list(l.strip()) for l in f]
+            raw = [list(map(convert_to_type, list(l.strip()))) for l in f]
             assert all(len(l) == len(raw[0]) for l in raw)
         self.bounds = Coord(len(raw), len(raw[0]))
         self.raw = raw
@@ -18,7 +19,7 @@ class Grid:
             return self.raw[p.x][p.y]
         else: return None
 
-    def set(self, p: Coord, c: str):
+    def set(self, p: Coord, c: Any):
         if self.inbounds(p):
             self.raw[p.x][p.y] = c
             return True
