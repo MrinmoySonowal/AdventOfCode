4dc3ad93f8b4255cc2fbd4432c9ed2f2abae9e57
MrinmoySonowal <mrinmoy.mly.sonowal@gmail.com>
2024-12-12 01:14:59 +0000

    Adding is_even to math utils

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -1,7 +1,9 @@
 from math import floor, log10
 
-
 def count_digits(number):
     if number == 0:
         return 1
     return floor(log10(abs(number))) + 1
+
+def is_even(number):
+    return number % 2 == 0
